293470b308cb5b398011903c05caa18456ac608d
itsagha <rasyid.agha@gmail.com>
2025-10-22 00:59:05 +0700

    typo 2

--- services/authServices.tsx
+++ services/authServices.tsx
@@ -7,13 +7,13 @@ export function logoutUser() {
 }
 
 export const loginUser = async (payload: { email: string; password: string }) => {
-  const LOGIN_URL = process.env.NEXT_LOGIN_URL || "/auth/login";
+  const LOGIN_URL = process.env.NEXT_PUBLIC_LOGIN_URL || "/auth/login";
   const res = await api.post(LOGIN_URL, payload);
   return res.data;
 };
 
 export const registerUser = async (payload: { name: string; email: string; password: string }) => {
-  const REGISTER_URL = process.env.NEXT_REGISTER_URL || "/auth/register";
+  const REGISTER_URL = process.env.NEXT_PUBLIC_REGISTER_URL || "/auth/register";
   const res = await api.post(REGISTER_URL, payload);
   return res.data;
 };
